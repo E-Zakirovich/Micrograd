5c459f0199c74819b53fd6852a2d3dc759eaff55
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-01-14 16:54:55 +0500

    Value class is finished and pushed to main orign

--- src/app.py
+++ src/app.py
@@ -1,6 +1,44 @@
-import value
+from value import Value
 
-a = value.Value(2.0)
-b = value.Value(3.0)
-c = 0.0
-print(a.tanh())
+from graphviz import Digraph
+def trace(root):
+  nodes, edges = set(), set()
+  def build(v):
+    if v not in nodes:
+      nodes.add(v)
+      for child in v._prev:
+        edges.add((child, v))
+        build(child)
+  build(root)
+  return nodes, edges
+
+def draw_dot(root):
+  dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+  nodes, edges = trace(root)
+  for n in nodes:
+    uid = str(id(n))
+    dot.node(name=uid, label="{%s | data %.4f | grad %.4f}" % (n.label, n.data, n.grad), shape="record")
+    if n._operation:
+      dot.node(name = uid + n._operation, label = n._operation)
+      dot.edge(uid + n._operation, uid)
+  for n1, n2 in edges:
+    dot.edge(str(id(n1)), str(id(n2)) + n2._operation)
+
+  return dot
+
+x1 = Value(2.0, label="x1")
+x2 = Value(0.0, label="x2")
+
+w1 = Value(-3.0, label="w1")
+w2 = Value(1.0, label="w2")
+
+b = Value(6.8813735870195432, label="b")
+
+x1w1 = x1 * w1
+x2w2 = x2 * w2
+n = x1w1 + x2w2 + b
+out = n.tanh()
+
+out.backward()
+dot = draw_dot(out)
+dot.render("neuron_graph", view=True)
