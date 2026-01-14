8af71124cb9ff4ca2fc0a4bbda99e48a6d352b59
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-01-14 14:43:53 +0500

    I added all properties to value class

--- src/value.py
+++ src/value.py
@@ -1,6 +1,9 @@
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _operation="", label=""):
         self.data = data
+        self._prev = _children
+        self._operation = _operation
+        self.label = label
 
     def __repr__(self):
         return f"Value(data = {self.data} | type = {type(self.data)})"
