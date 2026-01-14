c2a49631421c3f48edd3bf9e65c8ca2798c71d27
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-01-14 14:47:26 +0500

    Now you can calculate addition operations.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import value
 
 a = value.Value(2.0)
+b = value.Value(3.0)
 
-print(a)
+print(a + b)
--- src/value.py
+++ src/value.py
@@ -1,9 +1,22 @@
 class Value:
     def __init__(self, data, _children=(), _operation="", label=""):
         self.data = data
-        self._prev = _children
+        self._prev = set(_children)
         self._operation = _operation
         self.label = label
 
     def __repr__(self):
         return f"Value(data = {self.data} | type = {type(self.data)})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(
+            data=self.data + other.data,
+            _children=(self, other),
+            _operation=" add ",
+        )
+        return out
+
+    def __radd__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        return self + other
